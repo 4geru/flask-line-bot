afdea6986a3377c66bb31d6c37ea7fe40bb3ec96
a4geru <is0248vx@ed.ritsumei.ac.jp>
2018-04-15 14:32:33 +0000

    cloud9に対応

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+# coding: UTF-8
 import os
 import sys
+import ast
+from os.path import join, dirname
 from flask import Flask, request, abort
+from dotenv import load_dotenv
+from linebot import LineBotApi
+from linebot.exceptions import LineBotApiError
+
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -9,8 +16,11 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 )
+
+load_dotenv(join(dirname(__file__), '.env'))
+
 
 app = Flask(__name__)
 
@@ -40,7 +50,6 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
@@ -52,10 +61,29 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-
+    body = request.get_data(as_text=True)
+    data = ast.literal_eval(body)
+    data = data["events"][0] 
+    # app.logger.info(data)
+    try:
+        profile = line_bot_api.get_profile(data['source']['userId'])
+        print(profile.display_name)
+        print(profile.user_id)
+        print(profile.picture_url)
+        
+        # line_bot_api.reply_message(
+        #     event.reply_token,
+        #     ImageSendMessage(
+        #         original_content_url="https://res.cloudinary.com/dzhcf23xd/image/upload/v1503874903/pf7u56fr8qoqtl6pfhph.jpg",
+        #         preview_image_url="https://res.cloudinary.com/dzhcf23xd/image/upload/v1503874903/pf7u56fr8qoqtl6pfhph.jpg",
+        # ))
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
+    except LineBotApiError as e:
+        app.logger.info(e)
+    
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host=os.getenv("IP", '0.0.0.0'), port=int(os.getenv("PORT", 8080)))
+    # app.run()
